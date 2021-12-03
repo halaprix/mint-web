165a05a9f74cbf328b580ae05750eaa2a2464f3e
CrateCrabs <95129108+CrateCrabs@users.noreply.github.com>
2021-12-03 19:11:19 +0100

    Add files via upload

---
+++ src/components/SampleWork.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Image, Center, Wrap, WrapItem } from "@chakra-ui/react";
+
+import Section from "./Section";
+import sampleWork from "../content/sample-work.json";
+
+const SampleWork = () => {
+  return (
+    <Section
+      id="sampleWork"
+      subtitle={sampleWork.subtitle}
+      justify="center"
+      maxW="100%"
+      py={20}
+      sx={{ backgroundColor: "gray", marginTop: 0 }}
+    >
+      <Center position="relative" w="100%" py={16}>
+        <Wrap
+          spacing={8}
+          position="relative"
+          maxW="1200px"
+          justify="center"
+          m="0 !imporant"
+        >
+          <WrapItem>
+            <Image
+              src="/static/0.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/281.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/4.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/173.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/175.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/234.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/317.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/2.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+          <WrapItem>
+            <Image
+              src="/static/279.png"
+              w="200px"
+              h="200px"
+              borderRadius="1rem"
+            />
+          </WrapItem>
+        </Wrap>
+      </Center>
+    </Section>
+  );
+};
+
+export default SampleWork;
---
+++ src/components/WalletConnectionArea.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link, Heading, Center, VStack, Image } from "@chakra-ui/react";
+
+import Home from "./Home";
+
+const WalletConnectionArea = ({
+  title,
+  cta,
+  backgroundImage,
+  candyMachineId,
+  config,
+  connection,
+  startDate,
+  treasury,
+  txTimeout,
+  ...rest
+}) => {
+  return (
+    <Center
+      id="cta"
+      // height of the spacing
+      p={[8, 8, 2, 2]}
+      w="100%"
+      minH="70vh"
+      position="relative"
+      overflowX="hidden"
+      bg="#778ca2"
+      //sx={{ backgroundImage: `url(${"/static/banner.jpg"})`, objectFit: "fill" }}
+    >
+      <VStack
+        p={[8, 8, 20, 20]}
+        w="100%"
+        maxW="100%"
+        borderRadius="1rem"
+        borderColor="transparent"
+        borderWidth=".25rem"
+        bg="transparent"
+        color="white"
+        textAlign="center"
+        _hover={{ transform: "scale(1.005)" }}
+        transition="transform 0.5s ease-out"
+        spacing={8}
+        position="relative"
+        //backgroundSize="20px 20px"
+        //sx={{ backgroundImage: `url(${"/static/logoBig.png"})`}}
+      >
+       
+        <Image
+              src="/static/logoBig.png"
+              
+              h="300px"
+              borderRadius="1rem"
+            />
+        <VStack>
+          <Heading
+            as="h2"
+            size="lg"
+            fontWeight={800}
+            maxW="500px"
+            lineHeight="tall"
+          >
+          
+          </Heading>
+        </VStack>
+
+        <VStack spacing={4}>
+          <Link href={cta.href}>
+            <Home
+              candyMachineId={candyMachineId}
+              config={config}
+              connection={connection}
+              startDate={startDate}
+              treasury={treasury}
+              txTimeout={txTimeout}
+            />
+          </Link>
+        </VStack>
+      </VStack>
+    </Center>
+  );
+};
+
+export default WalletConnectionArea;
